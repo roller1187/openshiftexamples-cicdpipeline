cd6d05fa44e6627076287549b0b1d1daa82e2394
Andres Romero <aromero@redhat.com>
2019-05-23 23:45:18 -0400

    Initial pipeline changes to add Prod env

--- pipeline_setup.sh
+++ pipeline_setup.sh
@@ -2,6 +2,7 @@
 oc new-project cicd --display-name="CI/CD"
 oc new-project pipeline-app --display-name="Pipeline Example - Build"
 oc new-project pipeline-app-staging --display-name="Pipeline Example - Staging"
+oc new-project pipeline-app-prod --display-name="Pipeline Example - Prod"
 
 # Switch to the cicd and create the pipeline build from a template
 oc project cicd
@@ -11,9 +12,11 @@ oc create -f ./pipeline_instant_embeded.yaml
 # Give this project an edit role on other related projects
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n pipeline-app
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n pipeline-app-staging
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n pipeline-app-prod
 
 # Give the other related projects the role to pull images from pipeline-app
 oc policy add-role-to-group system:image-puller system:serviceaccounts:pipeline-app-staging -n pipeline-app
+oc policy add-role-to-group system:image-puller system:serviceaccounts:pipeline-app-prod -n pipeline-app
 
 # Wait for Jenkins to start
 oc project cicd
@@ -26,7 +29,7 @@ do
 done
 echo "Yay, Jenkins is ready."
 echo "But we need to do one more thing because of a current limitation."
-echo "From the CI/CD project - open the Jenkins webconsole, Manage Jenkins->Configure System->OpenShift Jenkins Sync->Namespace and add 'pipeline-app pipeline-app-staging' to the list"
+echo "From the CI/CD project - open the Jenkins webconsole, Manage Jenkins->Configure System->OpenShift Jenkins Sync->Namespace and add 'pipeline-app pipeline-app-staging pipeline-app-prod' to the list"
 echo ""
 echo "Then you can start your pipeline with:"
 echo "> oc start-build -F openshiftexamples-cicdpipeline"
